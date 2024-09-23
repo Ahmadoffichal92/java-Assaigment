d4ad4fedff895f19a275127d2932c574221e228f
Hafiz Ahmad Raza <160213811+Ahmadoffichal92@users.noreply.github.com>
2024-09-23 05:37:51 -0700

    Add files via upload

---
+++ modeol 02/app.js
@@ -0,0 +1,253 @@
+// chapter alert 1
+
+// q no  1. Write a script to greet your website visitor using JS alert box.
+// 2. Write a script to display following message on your web
+// page:
+
+// alert("Error! Please enter a valid passwords")
+
+// Q 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+// anser 3
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+
+
+// Q no 4. Write a script to display following messages in sequence:
+
+// anser 4
+
+// alert("Welcom to js land..")
+  
+// alert(" Happy coding!")
+
+// Q no5. Generate the following message through browser’s
+// developer console:
+
+// answer 5
+// alert( " welcome to js land")
+//     console.log("Hello... I can run JS through my web browser's console");
+
+
+
+// Qno6. Make use of alerts in your new/existing HTML & CSS
+// project.
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+
+
+// answer q no 6 and 7
+
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <script>
+//     // a. Head
+//     alert("i am ahmad")
+// </script>
+// <body>
+//     <script>
+//         alert("my self java ")
+//     </script>
+//     <h1>java</h1>
+    
+
+//     <script src="app.js"></script>
+// </body>
+// </html>
+
+// // chapter variabl string  2
+
+// var username = "Ahmad Raza ";
+// alert (username) ;
+
+// var age = "20 ";
+// alert (age) ;
+
+// var colification = "12th ";
+// alert (colification) ;
+
+
+
+// // java  men hum number me inverted quma men string men ikhenge """ ye na bhi lagay to code chlta he jubke alpha kelea ""' ZARORI HE 
+// var abc =( 90)
+// abc=(80)
+// alert(abc)
+// var abc =( "ahmad12")
+// alert(abc)
+
+
+// // chapter 3 number
+
+// Q 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+// var age =("i am 20 years old")
+// alert(age)
+
+
+// Q no 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+// var visiter = ("you have visiter site 14 items")
+// alert(visiter)
+
+// Q NO3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+//  answer 3
+
+// Q no4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// answer 4
+
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1 * num2 ;
+// alert (result)
+// ...............................................................................................
+// assaigment 2
+// capter 4 legal and illigle 
+// q 1 2 3 
+
+// legal statment cavil case
+// var whatIsYourName =("my name is ahmad raza")
+// alert(whatIsYourName)
+// // isi tarah variable ka jo name he usko java ka keywords nahi de sakte jese var wagera
+// // space dena he legal  tariqah snack case
+// var whta_Is_Your_name = ("my name is ahmad")
+// alert(whta_Is_Your_name)
+
+
+// illigl statment
+// var var =( "hello")
+// alert{var}
+
+// var 1ahmad =( "hello") starte men number bhi nahi laga sakte 
+// alert{1ahmad}
+
+// var ah mad =( "hello") space bhi nahi 
+// alert{ah mad}
+
+
+
+// cahpter 05 assigment 2
+// Q 1
+// answer
+// var num1 = (3)
+// var num2 =(5)
+// alert (num1+num2)
+// var num1 = (3)
+// var num2 =(5)
+// alert (num1-num2)
+// var num1 = (3)
+// var num2 =(5)
+// alert (num1*num2)
+
+// Q2 or 3 dicriment or incrment
+
+// var num = (5)
+// num = num+1 
+// num = num+7
+
+// alert(num )
+
+
+
+
+
+
+
+
+
+
+
+
+// chapter 12 and 13 
+// QUESTION 01
+// code kare agar apn ebara alpha basd likha he to uper case warna lover cas ajya 
+// aik aisa code kere jsme ap number likhe to woh batade ke code number he isi tarah or sab 
+    // let char = '8';  // Example input
+    // let ascii = char.charCodeAt(0);
+    // if (ascii >= 48 && ascii <= 57) {
+    //     console.log("It's a number.");
+    // } else if (ascii >= 65 && ascii <= 90) {
+    //     console.log("It's an uppercase letter.");
+    // } else if (ascii >= 97 && ascii <= 122) {
+    //     console.log("It's a lowercase letter.");
+    // } else {
+    //     console.log("It's neither a number nor a letter.");
+    // }
+    //  QUESTION 02 
+    //  is tarah code kare ke phla vale or dosra same ho to eqal sho karway bara ho to bara chota h to chota karway
+    // aik jesa number agar user lagay to same number show karway
+    //     let num1 = +(prompt("Please Enter first number"));
+    //     let num2 = +(prompt("Please enter second number"));
+    //    if(num1 > num2){
+    //     console.log("The larger number is " + num1);
+    //     alert("The larger number is " + num1);
+    //    }
+    //    else if(num2 > num1)
+    //    {
+    //     console.log("The larger number is " + num2);
+    //     alert("The larger number is " + num2);
+    //    }
+    //    else {
+    //     console.log("Both integers are equaal");
+    //     alert("Both integers are equaal");
+    //    } 
+
+    // QUESTION 03
+    // aisa code kare ke agar + men nuber dale to psitive _men dale to nagitive or same 00 dale to zero show ho
+    
+        // let number = parseFloat(prompt("Enter any number"));
+        // if( number > 0){
+        //     alert("The entered number is positive number :" + number);
+        // }
+        // else if(number < 0){
+        //     alert("The entered number is negative number :" + number);
+        // }
+        // else {
+        //     alert("The entered number is zero :" +number);
+        // }
+
+        //  QUESTION 04
+ // is tarah code kare ke value value ke baraba ay to apka code tru or na ay to fals bataden or han vovel vovel ke barabar hun
+    //     let char = prompt("Enter a character:");  
+    //     if (char === 'a' || char === 'A') {
+    //     console.log(true);
+    //     } else if (char === 'e' || char === 'E') {
+    //     console.log(true);
+    //     } else if (char === 'c' || char === 'C') {
+    // console.log(true);
+    // } else if (char === 'o' || char === 'O') {
+    // console.log(true);
+    //     } else if (char === 'u' || char === 'U') {
+    // console.log(true);
+    // } else {
+    // console.log(false);
+    //   }
+    
+    // q 05 
+    // nahai samajh aya 
