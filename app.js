2327071b22806cefe821a81bcc4ed2cbebc054e7
Hafiz Ahmad Raza <160213811+Ahmadoffichal92@users.noreply.github.com>
2024-09-25 05:34:55 -0700

    java script assigment 3

--- app.js
+++ app.js
@@ -1,22 +1,490 @@
 // chapter alert 1
-var  hello = "Error! Please enter a valid passwords";
-alert("Error! Please enter a valid passwords")
-var num = 23232323;
-alert (9929292292);
 
-// chapter string  2
-var username = ("enter your name ") ;
-  username = "jhon doy" ;
-alert (username) ;
+// q no  1. Write a script to greet your website visitor using JS alert box.
+// 2. Write a script to display following message on your web
+// page:
 
-// java  men hum number me inverted quma men string men ikhenge """ ye na bhi lagay to code chlta he jubke alpha kelea ""' ZARORI HE 
-var abc = 2222; 
-abc = "10";
-alert(abc);
+// alert("Error! Please enter a valid passwords")
 
-// chapter 3 number
+// Q 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
 
-var num1 = 3;
-var num2 = 5;
-var result = num1 * num2 ;
-alert (result)
+// anser 3
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+
+
+// Q no 4. Write a script to display following messages in sequence:
+
+// anser 4
+
+// alert("Welcom to js land..")
+  
+// alert(" Happy coding!")
+
+// Q no5. Generate the following message through browser’s
+// developer console:
+
+// answer 5
+// alert( " welcome to js land")
+//     console.log("Hello... I can run JS through my web browser's console");
+
+
+
+// Qno6. Make use of alerts in your new/existing HTML & CSS
+// project.
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+
+
+// answer q no 6 and 7
+
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <script>
+//     // a. Head
+//     alert("i am ahmad")
+// </script>
+// <body>
+//     <script>
+//         alert("my self java ")
+//     </script>
+//     <h1>java</h1>
+    
+
+//     <script src="app.js"></script>
+// </body>
+// </html>
+
+// // chapter variabl string  2
+// q no 1 2 3 4 
+// 1. Declare a variable called username.
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// var username ;
+// var  myName = "Ahmad razz"
+// console.log(myName)
+// var massage ="Hello World";
+// alert(massage)
+// console.log(massage)
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+ 
+// var myName =" Ahmad raza "
+// var myAge = " 19 years old"
+// var corseName = " web and app development"
+// var  InstituteName = "Smit "
+// var Email = "blue555moon5556@gmail.com"
+// var Contact = "03211216765"
+// alert("My name is" +myName )
+// console.log("My name is" +myName)
+// alert(myAge)
+// console.log(myAge)
+// alert(InstituteName)
+// console.log(InstituteName)
+// alert(Email)
+// console.log(Email)
+// alert(Contact)
+// console.log(Contact)
+
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+// var manu =" PIZZA \n PIZZ \n PIZ \n PI \n P "
+// console.log(manu)
+// alert(manu)
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+// var Email = "blue555moon5556@gmail.com"
+// alert("My email address is : " + Email);
+// console.log( "myEmailAdrees"  + Email)
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+// var book =" A smarter way to learn JavaScript"
+// alert ( "I Am Lerninig The Book" +book);
+
+// 8. Write a script to display this in browser through JS
+// agar java se direct browser men kuch dikhana ho to phir ducument ka used karte he 
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+// var write = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(write)
+// console.log(write)
+// or dosra tariqah
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// extra info
+// // java  men hum number me inverted quma men string men ikhenge """ ye na bhi lagay to code chlta he jubke alpha kelea ""' ZARORI HE 
+// var abc =( 90)
+// abc=(80)
+// alert(abc)
+// var abc =( "ahmad12")
+// alert(abc)
+
+
+// // chapter 3 number
+
+// Q 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+// var age =(" 19 years old")
+// alert( "I am"+age)
+
+
+// Q no 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+// var visiter = ("you have visiter site 14 items")
+// alert(visiter)
+
+// Q NO3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+// var birthYear =" 2005"
+// document.write("My birth years is " +birthYear) 
+// document.write( "  Data type of ddeclared variabla is number") 
+// alert( "My birth year is " + birthYear)
+
+
+
+// Q no4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// answer 4
+
+// var visiterName = "jhon Doe"
+// var productTittle ="T-Shirt"
+// var Quantity = " 5 "
+// console.log(visiterName)
+// console.log(productTittle)
+// console.log(Quantity)
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ \Clothing store")
+// extra code 
+
+// var visitorName = " John Doe ";
+// var productTitle = " T-shirt ";
+// var Quantity = 5;
+
+// message = visitorName + "ordered " + Quantity + productTitle + "(s) on XYZ Clothing store.";
+// document.getElementById('order-message').innerText = message;
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1 * num2 ;
+// alert (result)
+// ...............................................................................................
+// assaigment 2
+// Declare 3 variables in one statement 
+// var a = 2, b = 3, c = 4
+
+// capter 4 legal and illigle 
+// Declare 5 legal and 5 illegal variables
+// q 1 2 3 
+
+// legal statment cavil case
+// var whatIsYourName =("my name is ahmad raza")
+// alert(whatIsYourName)
+// // isi tarah variable ka jo name he usko java ka keywords nahi de sakte jese var wagera
+// // space dena he legal  tariqah snack case
+// var whta_Is_Your_name = ("my name is ahmad")
+// alert(whta_Is_Your_name)
+
+
+// illigl statment
+// var var =( "hello")
+// alert{var}
+
+// var 1ahmad =( "hello") starte men number bhi nahi laga sakte 
+// alert{1ahmad}
+
+// var ah mad =( "hello") space bhi nahi 
+// alert{ah mad}
+
+        // let name = "Ahmad";
+        // let f_username = "RAza";snack case
+        // let interYourAge = 19;cavil case
+        // let _value = 100;
+        // let my_name = "Raza yt "
+        
+
+        // ILLEGAL VARIABLES
+
+        // let my-name = "HASSAN";
+        // let @username = "admin";
+        // let 2ndPlace = "second";
+        // let var = 10; 
+        // let break = "break";
+
+        // Display this in your browser
+        // document.write("Rules for naming JS variables <br> <br> <br> Varaible name can only contain numbers, $ and_ . For example $my, _name or name <br> variables must begin with a letter, $ or_ . For example :$name , _name or name <br> Variables name are case sensitive <br> Variables names should not be JS  keywords")
+
+
+// cahpter 05 assigment 2
+// Q 1
+// answer
+// var num1 = (3)
+// var num2 =(5)
+// alert (num1+num2)
+// var num1 = (3)
+// var num2 =(5)
+// alert (num1-num2)
+// var num1 = (3)
+// var num2 =(5)
+// alert (num1*num2)
+
+// Q2 or 3 dicriment or incrment
+
+// var num = (5)
+// num = num+1 
+// num = num+7
+
+// alert(num )
+
+
+
+
+
+
+
+
+
+
+
+
+// chapter 12 and 13 
+// QUESTION 01
+// code kare agar apn ebara alpha basd likha he to uper case warna lover cas ajya 
+// aik aisa code kere jsme ap number likhe to woh batade ke code number he isi tarah or sab 
+    // let char = 'A';  // Example input
+    // let ascii = char.charCodeAt(0);
+    // if (ascii >= 48 && ascii <= 57) {
+    //     console.log("It's a number.");
+    // } else if (ascii >= 65 && ascii <= 90) {
+    //     console.log("It's an uppercase letter.");
+    // } else if (ascii >= 97 && ascii <= 122) {
+    //     console.log("It's a lowercase letter.");
+    // } else {
+    //     console.log("It's neither a number nor a letter.");
+    // }
+    //  QUESTION 02 
+    //  is tarah code kare ke phla vale or dosra same ho to eqal sho karway bara ho to bara chota h to chota karway
+    // aik jesa number agar user lagay to same number show karway
+    //     let num1 = +(prompt("Please Enter first number"));
+    //     let num2 = +(prompt("Please enter second number"));
+    //    if(num1 > num2){
+    //     console.log("The larger number is " + num1);
+    //     alert("The larger number is " + num1);
+    //    }
+    //    else if(num2 > num1)
+    //    {
+    //     console.log("The larger number is " + num2);
+    //     alert("The larger number is " + num2);
+    //    }
+    //    else {
+    //     console.log("Both integers are equaal");
+    //     alert("Both integers are equaal");
+    //    } 
+
+    // QUESTION 03
+    // aisa code kare ke agar + men nuber dale to psitive _men dale to nagitive or same 00 dale to zero show ho
+    
+        // let number = parseFloat(prompt("Enter any number"));
+        // if( number > 0){
+        //     alert("The entered number is positive number :" + number);
+        // }
+        // else if(number < 0){
+        //     alert("The entered number is negative number :" + number);
+        // }
+        // else {
+        //     alert("The entered number is zero :" +number);
+        // }
+
+        //  QUESTION 04
+ // is tarah code kare ke value value ke baraba ay to apka code tru or na ay to fals bataden or han vovel vovel ke barabar hun
+    //     let char = prompt("Enter a character:");  
+    //     if (char === 'a' || char === 'A') {
+    //     console.log(true);
+    //     } else if (char === 'e' || char === 'E') {
+    //     console.log(true);
+    //     } else if (char === 'c' || char === 'C') {
+    // console.log(true);
+    // } else if (char === 'o' || char === 'O') {
+    // console.log(true);
+    //     } else if (char === 'u' || char === 'U') {
+    // console.log(true);
+    // } else {
+    // console.log(false);
+    //   }
+    
+    // q 05 
+    // nahai samajh aya 
+// .................................................................
+
+
+// assigment 3
+let products = [
+    {
+      id: 101,
+      title: "Sony LED 40 inch",
+      variations: [
+        { id: 1, color: "black", price: 50000, quantity: 5 },
+        { id: 2, color: "red", price: 52000, quantity: 3 },
+        { id: 3, color: "silver", price: 55000, quantity: 8 },
+        { id: 4, color: "white", price: 54000, quantity: 4 },
+        { id: 5, color: "blue", price: 51000, quantity: 2 },
+      ],
+      reviews: [
+        {
+          id: 1,
+          user: "Ahmad",
+          rating: 4.0,
+          title: "Good Product",
+          comments: "It is a very good product ....",
+          date: "06-02-2021",
+          status: true,
+        },
+        {
+          id: 2,
+          user: "Zubair",
+          rating: 4.5,
+          title: "Very Good Product",
+          comments: "Zubair: It is a very good product ....",
+          date: "05-02-2021",
+          status: false,
+        },
+        {
+          id: 3,
+          user: "Ali",
+          rating: 5.0,
+          title: "Excellent Product",
+          comments: "Ali: I absolutely love it!",
+          date: "04-02-2021",
+          status: true,
+        },
+      ],
+    },
+    {
+      id: 102,
+      title: "Mobile",
+      variations: [
+        { id: 1, color: "black", price: 50000, quantity: 5 },
+        { id: 2, color: "orange", price: 48000, quantity: 6 },
+        { id: 3, color: "green", price: 55000, quantity: 8 },
+        { id: 4, color: "purple", price: 53000, quantity: 3 },
+        { id: 5, color: "gold", price: 59000, quantity: 1 },
+      ],
+      reviews: [
+        {
+          id: 1,
+          user: "Ahmad",
+          rating: 4.0,
+          title: "Good Product",
+          comments: "It is a very good product ....",
+          date: "06-02-2021",
+          status: true,
+        },
+        {
+          id: 2,
+          user: "Zubair",
+          rating: 4.5,
+          title: "Very Good Product",
+          comments: "Zubair: It is a very good product ....",
+          date: "05-02-2021",
+          status: false,
+        },
+        {
+          id: 3,
+          user: "Ali",
+          rating: 5.0,
+          title: "Best Mobile",
+          comments: "Ali: Perfect for my needs!",
+          date: "04-02-2021",
+          status: true,
+        },
+      ],
+    },
+    {
+      id: 103,
+      title: "Headphones",
+      variations: [
+        { id: 1, color: "black", price: 3000, quantity: 15 },
+        { id: 2, color: "white", price: 3500, quantity: 10 },
+        { id: 3, color: "red", price: 3200, quantity: 5 },
+        { id: 4, color: "blue", price: 3400, quantity: 8 },
+        { id: 5, color: "green", price: 3100, quantity: 12 },
+      ],
+      reviews: [
+        {
+          id: 1,
+          user: "Samira",
+          rating: 4.2,
+          title: "Great Sound",
+          comments: "Samira: The sound quality is impressive.",
+          date: "10-02-2021",
+          status: true,
+        },
+        {
+          id: 2,
+          user: "Tariq",
+          rating: 4.0,
+          title: "Good for the Price",
+          comments: "Tariq: Comfortable and decent sound.",
+          date: "09-02-2021",
+          status: true,
+        },
+        {
+          id: 3,
+          user: "Zara",
+          rating: 4.7,
+          title: "Love Them!",
+          comments: "Zara: Best headphones for my workouts.",
+          date: "08-02-2021",
+          status: true,
+        },
+      ],
+    },
+  ];
+  
+  function title1 (products)
+  let array = [];
+  for (i = 0 ; i < products.length; i++){
+    array.push(products [i].title)
+  }
+  return array;
+  console.log(title(products));
+  // Exercise 1
+  // Get all product titles and display them in array format.
+  // Example: ['Sony LED 40 inch', 'Mobile', 'Bike']
+  // Try with function or without function
